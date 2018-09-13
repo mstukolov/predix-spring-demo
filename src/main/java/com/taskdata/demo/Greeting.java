a64c9d73fe0b7bc37fd36470c861efab5c64d6d0
mstukolov <maxim.stukolov@gmail.com>
2018-09-13 18:28:36 +0300

    Initial commit

---
+++ src/main/java/com/taskdata/demo/Greeting.java
@@ -0,0 +1,24 @@
+package com.taskdata.demo;
+
+/**
+ * Created by mstukolov on 13.09.2018.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
